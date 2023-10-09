4c6b180329e43ccb11b67998a2962bbef747954b
Topcreater <mubashar7964768@gmail.com>
2023-10-09 23:23:15 +0500

    some changeing in data adding

--- src/pages/CardSection/index.tsx
+++ src/pages/CardSection/index.tsx
@@ -1,25 +1,36 @@
-import { Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { Text, View, SafeAreaView, Image, TouchableOpacity, FlatList } from 'react-native'
+import React, { useState } from 'react'
 import { styles } from './style'
 import { useNavigation } from '@react-navigation/native'
 import useStore from '../../zustandStore/addfavorat'
+import CardDetails from '../CardDetails'
 const CardSection = () => {
     const navigation = useNavigation<any>()
     const data = useStore((state: any) => state.data);
     const removeData = useStore((state: any) => state.removeData);
+    const [clickedItemIndex, setClickedItemIndex] = useState(null);
+
+    const showDropDownMenu = (index: any) => {
+        setClickedItemIndex(index === clickedItemIndex ? null : index);
+    };
+
     console.log(data)
     const handleRemoveData = (item: any) => {
         removeData(item);
-
     };
+    const renderItem = ({ item, index }: any) => (
+        <CardDetails item={item} index={index} showDropDownMenu={showDropDownMenu} clickedItemIndex={clickedItemIndex} data={data} setClickedItemIndex={setClickedItemIndex} handleRemoveData={handleRemoveData} />
+    );
     return (
-        <SafeAreaView style={{ backgroundColor: 'black', flex: 1 }}>
-            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-                <View>
-                    <Text style={styles.textColor}>Card</Text>
-                </View>
-            </TouchableOpacity>
-        </SafeAreaView>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <FlatList
+                data={data} style={{ marginTop: 150 }}
+                ListEmptyComponent={
+                    <Text >Nothing is downloaded.....</Text>
+                }
+                renderItem={renderItem}
+            />
+        </View>
     )
 }
 export default CardSection;
